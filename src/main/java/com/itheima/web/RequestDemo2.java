dd1e02b3fe183d296d0d38d5441ba6c5c0475769
zsg <1467514028@qq.com>
2022-07-23 11:35:29 +0800

    first

---
+++ src/main/java/com/itheima/web/RequestDemo2.java
@@ -0,0 +1,75 @@
+package com.itheima.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
+
+@WebServlet("/req2")
+public class RequestDemo2 extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("get....");
+        //1. 获取所有参数的Map集合
+        Map<String, String[]> map = req.getParameterMap();
+        for (String key : map.keySet()) {
+            // username:zhangsan lisi
+            System.out.print(key + ":");
+
+            //获取值
+            String[] values = map.get(key);
+            for (String value : values) {
+                System.out.print(value + " ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("------------");
+        String[] hobbies = req.getParameterValues("hobby");
+        for (String hobby : hobbies) {
+            System.out.println(hobby);
+        }
+
+        System.out.println("------------");
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        System.out.println(username);
+        System.out.println(password);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println("Post....");
+        //1. 获取所有参数的Map集合
+        Map<String, String[]> map = req.getParameterMap();
+        for (String key : map.keySet()) {
+            // username:zhangsan lisi
+            System.out.print(key + ":");
+
+            //获取值
+            String[] values = map.get(key);
+            for (String value : values) {
+                System.out.print(value + " ");
+            }
+
+            System.out.println();
+        }
+
+        System.out.println("------------");
+        String[] hobbies = req.getParameterValues("hobby");
+        for (String hobby : hobbies) {
+            System.out.println(hobby);
+        }
+
+        System.out.println("------------");
+
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        System.out.println(username);
+        System.out.println(password);
+    }
+}
